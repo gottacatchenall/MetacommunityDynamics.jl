8bcb0b202461d20584d3ce1406919dba9bb06d10
michael catchen <michael.catchen@colorado.edu>
2021-07-19 10:03:46 -0400

    food web example minimal functionality

--- examples/foodwebs/01_simple_foodweb.jl
+++ examples/foodwebs/01_simple_foodweb.jl
@@ -18,9 +18,6 @@ trophicdict = trophic_level(foodweb)  # returns a dictionary
 resourcenames = filter(s -> trophicdict[String(s)] == 1.0, species(speciespool))
 consumernames = filter(s -> trophicdict[String(s)] != 1.0, species(speciespool))
 
-# I should store mass as auxilary data here probably because it can't change over time,
-# but trait layers will be important for the next example
-
 masses = Dict()
 for (k,v) in zip(keys(trophicdict), values(trophicdict))
     masses[Symbol(k)] = 2^v
@@ -28,30 +25,49 @@ end
 masses = NamedTuple(masses)
     
 consumermodel = 
-    FoodWebEating(consumernames, resourcenames, LotkaVolterra(1.5), speciespool.metaweb) +
+    FoodWebEating(consumernames, resourcenames, LotkaVolterra(0.03), speciespool.metaweb) +
   #  AdjacentBernoulliDispersal(consumernames, DispersalKernel(radius=1), 0.1) +
-    LinearMortality(consumernames, 0.2);
+    LinearMortality(consumernames, 0.01);
 
 plantmodel = 
     LogisticGrowth(resourcenames) +
   #  AdjacentBernoulliDispersal(resourcenames, DispersalKernel(radius=3), 0.1) + 
-    LinearMortality(resourcenames, 0.1);
+    LinearMortality(resourcenames, 0.01);
 
 
 fullmodel = consumermodel + plantmodel
-
-
-# this should be a dictionary/namedtuple of :a => Matrix for a 
 
 init = NamedTuple(merge(
     rand(Biomass, resourcenames, Exponential(10), dims...),
     rand(Biomass, consumernames, Exponential(10), dims...)));
 
-arrayout = ArrayOutput(init, tspan=1:10, aux=(masses=masses, speciespool=speciespool,))
+arrayout = ArrayOutput(init, tspan=1:1000, aux=(masses=masses, speciespool=speciespool,))
 @time sim!(arrayout, fullmodel) 
 
 
-arrayout[2]
 
 
-m = (s0=5, s1=10)
+"""
+    plots : 
+"""
+
+mat = zeros(number_of_species, 1000)
+
+spnames = []
+for t = 1:1000
+  ind = 1
+
+  for (k,v) in zip(keys(arrayout[t]), values(arrayout[t]))
+    mat[ind, t] = sum(v)
+    push!(spnames, k)
+    ind += 1
+  end  
+end
+
+
+plot(mat', legend=:outerright, frame=:box, dpi=150)
+
+xaxis!("timestep")
+yaxis!("biomass") 
+
+savefig("foodwebtraj.png")
