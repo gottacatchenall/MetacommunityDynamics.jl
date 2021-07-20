5f393821bad872cf9d3caf999b88233aa1faf914
michael catchen <michael.catchen@colorado.edu>
2021-07-20 15:03:53 -0400

    LG constructor fix and LV gif updates

--- examples/lotkavolterra_biomass/lotka_volterra_gif.jl
+++ examples/lotkavolterra_biomass/lotka_volterra_gif.jl
@@ -1,21 +1,21 @@
 using MetacommunityDynamics
 using DynamicGrids
-using DynamicGridsGtk
-using Dispersal
 using Distributions 
 using ColorSchemes 
+using Dispersal: DispersalKernel, OutwardsDispersal
 using Plots
 
 consumermodel = 
     Eating{Tuple{:C,:R}}(
-        functionalresponse=LotkaVolterra(0.1), 
+        functionalresponse=LotkaVolterra(0.2), 
         dt=0.1) +
-    AdjacentBernoulliDispersal{:C}(DispersalKernel(radius=2), 0.5) + 
+        OutwardsDispersal{:C}(DispersalKernel(radius=3)) + 
+    #RandomExtinction{:C}(0.1) + 
     LinearMortality{:C}(0.1);
 
 resourcemodel = 
-    MetacommunityDynamics.LogisticGrowth{:R}(λ=1.1, K=200., dt=0.1) +
-    AdjacentBernoulliDispersal{:R}(DispersalKernel(radius=1), 0.2);
+    LogisticGrowth{:R}( λ=1.1, K=200., dt=0.1) +
+    OutwardsDispersal{:R}(DispersalKernel(radius=5));
 
 model = resourcemodel + consumermodel
 
@@ -37,31 +37,28 @@ end
 
 # wave initial condition 
 latticesize = 250
+
+initresource = rand(Biomass, Exponential(10.) ,latticesize, latticesize)
+
 initconsumer = zeros(Biomass, latticesize, latticesize)
-initresource = zeros(Biomass,  latticesize, latticesize)
-
-
-rci = rand(DiscreteUniform(1, latticesize), (50, 2) )
-rri = rand(DiscreteUniform(1, latticesize), (300, 2) )
-
+rci = rand(DiscreteUniform(1, latticesize), (100, 2) )
 map( i -> initconsumer[i...] = 10., rci)
-map( i -> initresource[i...] = 10., rri)
 
 
-gtkout = GtkOutput((C=initconsumer, R=initresource ), tspan=1:1000, scheme=(ColorSchemes.Blues_5, ColorSchemes.Greens_4),  minval=(0,0), maxval=(3,10),)
-sim!(gtkout, model)
 
+arrayout = ArrayOutput((C=initconsumer, R=initresource ), tspan=1:500)
+@time sim!(arrayout, model)
 
-arrayout = ArrayOutput((C=initconsumer, R=initresource ), tspan=1:750)
-@time sim!(arrayout, model) # > roughly 1s on single core laptor
-plot_lv_timeseries(arrayout) # reaches steady state around timestep 1000
+max(arrayout[300][:R]...)
+
+plot_lv_timeseries(arrayout) 
 
 gifoutput = GifOutput((C=initconsumer, R=initresource ),
     filename="./docs/static/spicylv.gif", 
     tspan=1:750, 
     fps=25, 
-    minval=(0,0), maxval=(3,10),
+    minval=(0,0), maxval=(10, 30),
     scheme = (ColorSchemes.Blues_5, ColorSchemes.Greens_4),
     padval = 100
 )
-sim!(gifoutput, model)
+@time sim!(gifoutput, model)
