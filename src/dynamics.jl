16c06cd7878adbfcaa99dee925ef1b957d7f4f15
michael <michael@pop-os.localdomain>
2023-06-11 14:41:53 -0400

    dynamics

---
+++ src/dynamics.jl
@@ -0,0 +1,17 @@
+
+
+function dynamics(X)
+    
+    # X is a matrix with species on rows and patches on columns
+
+    # Competition: 
+    # ---------------------------------------
+    # For species `s` in patch `i`
+    # dXₛᵢ = Xₛᵢ * (rₛᵢ)/(1 + ∑ⱼ αⱼᵢ) + Iᵢₛ - Eᵢₛ
+
+    # Trophism
+    #
+    # dXₛᵢ = Xₛᵢ * (rₛᵢ)/(1 + ∑ⱼ αⱼᵢ) 
+
+
+end
