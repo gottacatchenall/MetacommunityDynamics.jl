b6a7ba5cc2344c2dae0e87885091cfb4d886ed4e
michael <michael@pop-os.localdomain>
2023-06-12 14:54:47 -0400

    :pencil:

--- src/dispersal/kernel.jl
+++ src/dispersal/kernel.jl
@@ -2,15 +2,13 @@
 @kwdef struct DispersalKernel
     func::Function = (x, decay) -> exp(-x * decay) # a function mapping (x, decay) to a value in [0,1]
     decay = 3.0 # a positive real number 
-    threshold = 0.01 # cutoff threshold for a value of func to be considered 0
+    max_distance = 1.0 # cutoff threshold for a value of func to be considered 0
 end
 function (dk::DispersalKernel)(x)
-    f = dk.func(x, dk.decay)
-    f > dk.threshold ? f : 0
+    x > dk.max_distance ? 0 : dk.func(x, dk.decay)
 end
 
 Base.string(kern::DispersalKernel) = """
-{bold}Kernel: {/bold}{green}$(kern.func){/green}
 {bold}Decay: {/bold}{yellow}$(kern.decay){/yellow}
 {bold}Threshold: {/bold}{#a686eb}$(kern.threshold){/#a686eb}
 """
