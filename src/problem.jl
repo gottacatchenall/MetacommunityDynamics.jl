1d77f3bcce2983a081508f5252a406cf968c0c2e
michael <michael@pop-os.localdomain>
2023-06-15 16:41:22 -0400

    :pencil:

--- src/problem.jl
+++ src/problem.jl
@@ -1,4 +1,4 @@
-struct Problem{T <: SciMLBase.AbstractODEProblem}
+struct Problem{T <: SciMLBase.AbstractDEProblem}
     model::Model
     prob::T
     tspan
@@ -31,19 +31,31 @@ function problem(m::Model, ::Type{Deterministic}; tspan=(0,100), u0=nothing)
 
     Problem(m, prob(f, u0, tspan, ()) , tspan, u0, Missing())
 end
+
+
+function problem(m::Model, gd::GaussianDrift; tspan=(0,100), u0=nothing)
+    
+
+    prob = discreteness(m) == MetacommunityDynamics.Continuous ? ODEProblem : DiscreteProblem
+    
+
+
+    # this should dispatch on whether a spatialgraph was provided
+    f, g = factory(m,gd) 
+    u0 = isnothing(u0) ? initial(m) : u0
+
+    # TODO replace params here with a value loaded via
+    # parameters(m). 
+
+    # This will also enable injection of environment dependent variables here
+    # for the spatial version of this method 
+    
+    Problem(m, prob(f, g, u0, tspan, ()) , tspan, u0, Missing())
+end
+
 function simulate(p::Problem)
     sol = solve(p.prob, saveat=(p.tspan[1]:1:p.tspan[2]))
     Trajectory(p, sol)
 end
 
 
-
-
-function model(m::Model, ::Deterministic, ::SpatialGraph)
-        
-end
-
-
-function model(m::Model, g::GaussianDrift, ::SpatialGraph)
-
-end 
