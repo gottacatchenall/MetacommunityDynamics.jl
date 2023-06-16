e88deb260cd9a19cfa21c5f56f373669e987c239
michael <michael@pop-os.localdomain>
2023-06-16 14:50:22 -0400

    :pencil:

--- src/models/bevertonholt.jl
+++ src/models/bevertonholt.jl
@@ -4,7 +4,14 @@
 The [Beverton-Holt
 model](https://en.wikipedia.org/wiki/Beverton%E2%80%93Holt_model) is a
 discrete-time, deterministic model of population dynamics. It is commonly
-interpreted as a discrete-time version of the logistic model.  
+interpreted as a discrete-time version of the logistic model.
+
+It is described by 
+
+$$N_{t+1} =\frac{R_0 M}{N_t + M}N_t$$
+
+where $K = (R\_0 - 1)M$ is the carrying capacity.
+
 """
 @kwdef struct BevertonHolt{F<:Number} <: Model
     R₀::F = 1.2
