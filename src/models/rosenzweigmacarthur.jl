d0f59b489b64d7f3f779ca5b9c3f4f5ffcdd6fad
michael <michael@pop-os.localdomain>
2023-06-16 14:57:23 -0400

    :pencil:

--- src/models/rosenzweigmacarthur.jl
+++ src/models/rosenzweigmacarthur.jl
@@ -4,8 +4,8 @@
     
     Dynamics given by
 
-    $$\frac{dR}{dt} = \lambda R \bigg(1 - \frac{R}{K}\bigg) - \frac{\alpha CR}{1 +\alpha \eta R}$$
-    $$\frac{dC}{dt} = \frac{\alpha CR}{1 + \alpha \eta R} - \gamma   C$$
+    ``\frac{dR}{dt} = \lambda R \bigg(1 - \frac{R}{K}\bigg) - \frac{\alpha CR}{1 +\alpha \eta R}``
+    ``\frac{dC}{dt} = \frac{\alpha CR}{1 + \alpha \eta R} - \gamma   C``
 
 """
 @kwdef struct RosenzweigMacArthur{S,T<:Number} <: Model
