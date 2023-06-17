6e8abf6697df38b65327af774e6774cdcddc00f2
michael <michael@pop-os.localdomain>
2023-06-17 11:46:23 -0400

    :sparkles: stochasticity

--- src/MetacommunityDynamics.jl
+++ src/MetacommunityDynamics.jl
@@ -23,9 +23,17 @@ module MetacommunityDynamics
         σ::T = 0.1
     end
 
-    function ∂w(gd::GaussianDrift, x)
+    function ∂w(gd::GaussianDrift, x::T) where T<:Real
         gd.σ
     end
+    function ∂w(gd::GaussianDrift, x::Vector{T}) where T<:Real
+        [gd.σ for _ in 1:length(x)]
+    end
+    function ∂w(gd::GaussianDrift, x::Matrix{T}) where T<:Real
+        σ = similar(x)
+        σ .= gd.σ
+    end
+    
     
     export Stochasticity, Deterministic, GaussianDrift
 
