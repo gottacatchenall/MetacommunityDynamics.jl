ac3a1a8da40dfb6a2648976bcd4d907d3c38e157
michael <michael@pop-os.localdomain>
2023-06-19 17:05:06 -0400

    :facepalm:

--- src/models/rosenzweigmacarthur.jl
+++ src/models/rosenzweigmacarthur.jl
@@ -6,7 +6,7 @@ Dynamics given by
 
 ``\\frac{dR}{dt} = \\lambda R \\bigg(1 - \\frac{R}{K}\\bigg) - \\frac{\\alpha CR}{1 +\\alpha \\eta R}``
 
-``\\frac{dC}{dt} = \\frac{\\alpha CR}{1 + \\alpha \\eta R} - \\gamma   C``
+``\\frac{dC}{dt} = \\beta \\frac{\\alpha CR}{1 + \\alpha \\eta R} - \\gamma   C``
 
 
 
