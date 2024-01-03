d13253521f62eaadd66ee680de1d89e3f6241098
michael d. catchen <mdcatchen@gmail.com>
2024-01-03 12:33:46 -0500

    module name reversion:

---
+++ src/MetacommunityDynamics.jl
@@ -0,0 +1,13 @@
+module MetacommunityDynamics
+    using Distributions
+    using Term
+    using UnicodePlots
+    using Distances
+    using NeutralLandscapes
+    using DifferentialEquations
+    using CairoMakie
+    using TestItems
+    
+    include("includes.jl")
+    include("exports.jl")
+end # module
